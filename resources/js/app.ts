9e2e08b7989e569ac4ed06b6b8f6fa8f782fbe85
Daniel Iván López García <lopez.garcia1319@gmail.com>
2026-03-31 19:09:25 -0600

    ✨ configurando vue query

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,6 +1,7 @@
 import './bootstrap';
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import { VueQueryPlugin } from '@tanstack/vue-query'
 import App from './src/App.vue';
 import router from './src/router/router';
 import ui from '@nuxt/ui/vue-plugin';
@@ -11,4 +12,5 @@ const app = createApp(App);
 app.use(createPinia());
 app.use(router);
 app.use(ui);
+app.use(VueQueryPlugin);
 app.mount('#app');
